f08f436f5cd89468bf07492afd2df544e4b89e84
Siva <imsivahere@gmail.com>
2025-05-24 10:52:55 +0530

    azure function demo changes

--- learningapp/Pages/Index.cshtml.cs
+++ learningapp/Pages/Index.cshtml.cs
@@ -1,37 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace learningapp.Pages;
 
 public class IndexModel : PageModel
 {
-     public List<Course> Courses=new List<Course>();
+    public List<Course> Courses = new List<Course>();
     private readonly ILogger<IndexModel> _logger;
     private IConfiguration _configuration;
-    public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
-        _configuration=configuration;
+        _configuration = configuration;
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGet()
     {
-       
-        string connectionString = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")!;
-        var sqlConnection = new SqlConnection(connectionString);
-        sqlConnection.Open();
 
-        var sqlcommand = new SqlCommand(
-        "SELECT CourseID,CourseName,Rating FROM Course;",sqlConnection);
-         using (SqlDataReader sqlDatareader = sqlcommand.ExecuteReader())
-         {
-             while (sqlDatareader.Read())
-                {
-                    Courses.Add(new Course() {CourseID=Int32.Parse(sqlDatareader["CourseID"].ToString()),
-                    CourseName=sqlDatareader["CourseName"].ToString(),
-                    Rating=Decimal.Parse(sqlDatareader["Rating"].ToString())});
-                }
-         }
+        string? functionURL = _configuration.GetValue<string>("MySettings:FunctionUrl");
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(functionURL);
+            string content = await response.Content.ReadAsStringAsync();
+            var deserializedCourses = JsonConvert.DeserializeObject<List<Course>>(content);
+
+            if (deserializedCourses != null)
+            {
+                Courses = deserializedCourses;
+            }
+
+            return Page();
+        }
+
     }
 }
